781c44cc7062e0893e3fcd780d7614daa7503f07
Miguel Herreros <19m33m99@gmail.com>
2025-06-09 10:12:05 +0200

    Bloqueo solucionado

--- Búsqueda del Tesoro.py
+++ Búsqueda del Tesoro.py
@@ -20,9 +20,9 @@ if __name__ == "__main__":
     # resto de nodos la posición inicial de los jugadores.
     key_loc = comm.scatter(coords if rank == 0 else None, root = 0)
     if rank == 0:
-        print(f"Tesoro en {key_loc}")
+        print(f"Tesoro en {key_loc}", flush= True)
     else:
-        print(f"Proceso {rank}, casilla inicial {key_loc}")
+        print(f"Proceso {rank}, casilla inicial {key_loc}", flush= True)
 
     if rank != 0:
         # Inicializamos una lista con las posiciones que ha visitado cada equipo en los nodos hijo.
@@ -34,13 +34,13 @@ if __name__ == "__main__":
 
     while not treasure_found:
         if rank == 0:
-            # Recivimos en el nodo maestro la posición de los distintos jugadores 
+            # Recivimos en el nodo maestro la posición de los distintos jugadores
             # y comprobamos si alguno ha ganado el juego
             for i in range(1,5):
                 current_loc = comm.recv(source = i)
                 if current_loc == key_loc:
                     winning_team = "Par" if i % 2 == 0 else "Impar"
-                    print(f"Gana el equipo {winning_team}")
+                    print(f"Gana el equipo {winning_team}", flush= True)
                     treasure_found = comm.bcast(True, root = 0)
                 else:
                     comm.send(False, dest = i, tag = 0)
@@ -49,22 +49,29 @@ if __name__ == "__main__":
             # Es decir, un movimiento que no se salga del tablero y que no vaya a una casilla que
             # por la que ya haya pasado alguien de su equipo.
             legal_move = False
-            while not legal_move:
+            tried_directions = 0
+            while not legal_move and tried_directions < 4:
                 direction = np.random.randint(4)
                 match direction:
                     case 0:
                         test_loc = (key_loc[0] + 1, key_loc[1])
+                        tried_directions += 1
                     case 1:
                         test_loc = (key_loc[0] - 1, key_loc[1])
+                        tried_directions += 1
                     case 2:
                         test_loc = (key_loc[0], key_loc[1] + 1)
+                        tried_directions += 1
                     case _:
                         test_loc = (key_loc[0], key_loc[1] - 1)
-                if max(test_loc) < 20 and min(test_loc) >= 0 and test_loc not in visited_by_team:
+                        tried_directions += 1
+                if max(test_loc) < 20 and min(test_loc) >= 0 and (test_loc not in visited_by_team or tried_directions >= 4):
                     legal_move = True
                     key_loc = test_loc
-            print(f"Proceso {rank}, nueva casilla {key_loc}")
-            # Enviamos la nueva posición al nodo maestro para comprobar si hemos encontrado el tesoro.
+
+            print(f"Proceso {rank}, nueva casilla {key_loc}", flush= True)
+            # Enviamos la nueva posición al nodo maestro
+            # para comprobar si hemos encontrado el tesoro.
             comm.send(key_loc, dest = 0, tag = 0)
             have_i_found_it = comm.recv(source = 0)
             # Si no se ha encontrado, actualizamos la lista de posiciones comprobadas
